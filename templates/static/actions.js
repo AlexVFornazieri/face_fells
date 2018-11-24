0a18a9aecec1a42249864f217a0ca253154fad88
Alex Vidotti Fornazieri <alex@robilar.com.br>
2018-11-24 00:42:37 -0200

    Amost there

--- templates/static/actions.js
+++ templates/static/actions.js
@@ -1,13 +1,36 @@
-var uploadProgress = document.getElementById('uploadProgress');
-var fileInput = document.getElementById('fileInput');
+// likelihoods intensity enum
+const likelihoods = ['UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE', 'LIKELY', 'VERY_LIKELY'];
+const likelihoodsText = ['Desconhecido', 'Muito improvável', 'Improvável', 'Possível', 'Provavél', 'Muito provavél'];
+
+// Returned feelings list
+const fells = ['joy', 'sorrow', 'anger', 'surprise', 'underExposed', 'blurred'];
+const fellsText = ['Feliz', 'Triste', 'Com raiva', 'Surpresa', 'Pouca exposixão', 'Desfocada'];
+
+// DOM elements
+const uploadProgress = document.getElementById('uploadProgress');
+const fileInput = document.getElementById('fileInput');
+const analysedImage = document.getElementById('analysedImage');
+const imageFeels = document.getElementById('imageFeels');
+
 
 // The user select a file
 fileInput.addEventListener('change', function (e) {
-  var file = e.target.files[0];
-  if (!file) return;
+  const file = e.target.files[0];
+  if (file) {
+    imageFeels.innerHTML = '';
+    fileInput.style.display = 'none';
+    analysedImage.style.display = 'none';
+    uploadFile(file);
+  }
+});
 
-  var storageRef = firebase.storage().ref('uploads/' + file.name);
-  var task = storageRef.put(file);
+/**
+ * Upload a file to Firebase Storage
+ * @param file
+ */
+function uploadFile(file) {
+  const storageRef = firebase.storage().ref('uploads/' + file.name);
+  const task = storageRef.put(file);
 
   task.on('state_changed',
     function progress(snap) {
@@ -16,72 +39,33 @@ fileInput.addEventListener('change', function (e) {
     },
 
     function error(err) {
-      console.error(err)
+      console.error(err);
     },
 
     function complete() {
+      task.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+        analysedImage.src = downloadURL;
+      });
       const storedPath = task.snapshot.ref.fullPath;
       // Create request to vision
-      subimitToVision(storedPath);
+      submitToVision(storedPath);
 
       // Clear input and progress
-      uploadProgress.value = 0;
+      uploadProgress.removeAttribute('value');
       fileInput.value = "";
     }
   )
-});
-
-function processResult(response) {
-  const annotations = response.responses[0].faceAnnotations[0];
-
-  const likelihoods = ['UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE', 'LIKELY', 'VERY_LIKELY'];
-  const likelihoodsText = ['Desconhecido', 'Muito improvável', 'Improvável', 'Possível', 'Provavél', 'Muito provavél'];
-  const fells = ['joy', 'sorrow', 'anger', 'surprise', 'underExposed', 'blurred'];
-  const fellsText = ['Feliz', 'Triste', 'Com raiva', 'Surpresa', 'Pouca exposixão', 'Desfocada'];
-
-  var results = [];
-
-  for (var x = 0; x < fells.length; x++) {
-    const fell = fells[x];
-    const likelihood = annotations[fell + 'Likelihood'];
-    results.push({
-      feel: fell,
-      score: likelihoods.indexOf(likelihood)
-    })
-  }
-
-  // Order by likely
-  results.sort(dynamicSort('score'));
-  showResults(results)
 }
 
-function showResults(results) {
-  const imageFeels = document.getElementById('imageFeels');
-  imageFeels.innerHTML = '';
 
-  for (var x = results.length - 1; x >=0 ; x--) {
-    const result = results[x];
+/**
+ * Vision annotate faces detection
+ * @param path
+ */
+function submitToVision(path) {
+  const API_KEY = 'AIzaSyDLns0jY32H1bhq-8ESm_LD20XrTTseSA0';
 
-    if (result.score >= 2) {
-      var span = document.createElement('span');
-
-      var className = result.feel;
-      if (result.score === 5) {
-        className += ' very';
-      }
-      span.className = className;
-
-      imageFeels.appendChild(span)
-    }
-  }
-
-  console.log(results)
-}
-
-function subimitToVision(path) {
-  var API_KEY = 'AIzaSyDLns0jY32H1bhq-8ESm_LD20XrTTseSA0';
-
-  var requestBody =
+  const requestBody =
     {
       "requests": [
         {
@@ -114,6 +98,67 @@ function subimitToVision(path) {
   xhttp.send(JSON.stringify(requestBody));
 }
 
+/**
+ * Process Vision result
+ * @param response
+ */
+function processResult(response) {
+  if (!response.responses[0].faceAnnotations) {
+    console.log("Não encontrado faces");
+    // TODO Inform user
+    return
+  }
+  const annotations = response.responses[0].faceAnnotations[0];
+
+  var results = [];
+
+  for (var x = 0; x < fells.length; x++) {
+    const fell = fells[x];
+    const likelihood = annotations[fell + 'Likelihood'];
+    results.push({
+      feel: fell,
+      score: likelihoods.indexOf(likelihood)
+    })
+  }
+
+  // Order by likely
+  results.sort(dynamicSort('score'));
+  showResults(results.reverse())
+}
+
+/**
+ * Show result in DOM
+ * @param results
+ */
+function showResults(results) {
+  imageFeels.innerHTML = '';
+  uploadProgress.style.display = 'none';
+  analysedImage.style.display = 'block';
+
+  if (results[0].score >= 2) {
+    appendFeel(results[0].feel);
+    if (results[1].score === 5) {
+      appendFeel(results[1].feel)
+    }
+  } else {
+    appendFeel('unknown')
+  }
+
+  console.log(results)
+}
+
+function appendFeel(feel) {
+  var span = document.createElement('span');
+  var className = feel;
+  span.className = className;
+  imageFeels.appendChild(span);
+}
+
+/**
+ * Helper to sort array by a object property
+ * @param property
+ * @returns {function(*, *): number}
+ */
 function dynamicSort(property) {
   var sortOrder = 1;
   if (property[0] === "-") {
